373c3e562cb84dcedd240ff004743ac6388b804c
Jakub Mrówczyński <jakub@mrowczynski.net>
2024-03-24 19:40:20 +0100

    setup running commands on ec2 instance

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 sudo apt install -y docker.io
 sudo apt install -y awscli
 
-aws ecr get-login-password --region eu-central-1 | sudo docker login --username AWS --password-stdin 944750375340.dkr.ecr.eu-central-1.amazonaws.com
+aws ecr get-login-password --region eu-central-1 | sudo docker login --username AWS --password-stdin 637423548204.dkr.ecr.eu-central-1.amazonaws.com
 
 
 sudo docker pull 637423548204.dkr.ecr.eu-central-1.amazonaws.com/tsd-project/fastapi:latest
