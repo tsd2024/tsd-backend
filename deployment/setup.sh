75e341acd7902828683e59a3ae30f113f29c4af8
Jakub Mrówczyński <jakub@mrowczynski.net>
2024-03-24 19:03:45 +0100

    initial infra structure

---
+++ deployment/setup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+sudo apt update -y
+sudo apt install -y docker.io
+sudo apt install -y awscli
+
+aws ecr get-login-password --region eu-central-1 | sudo docker login --username AWS --password-stdin 944750375340.dkr.ecr.eu-central-1.amazonaws.com
+
+
+sudo docker pull 637423548204.dkr.ecr.eu-central-1.amazonaws.com/tsd-project/fastapi:latest
+
+sudo docker pull 637423548204.dkr.ecr.eu-central-1.amazonaws.com/tsd-project/redis:latest
+
+sudo docker pull 637423548204.dkr.ecr.eu-central-1.amazonaws.com/tsd-project/postgres:latest
+
