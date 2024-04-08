89a67e27abb05ddc828afa413b36f19154eb7998
miloszmat123 <myyylek@gmail.com>
2024-04-09 00:42:40 +0200

    add sending info on websocket

--- src/domain/redis_connector/redis_handler.py
+++ src/domain/redis_connector/redis_handler.py
@@ -30,6 +30,11 @@ class RedisHandler:
                 lobby_data = json.loads(lobby_data.decode())
                 max_players = lobby_data['lobby_metadata']['max_players']
                 players = lobby_data['players']
+
+                player_ids = [player['player_id'] for player in players]
+                if player_id in player_ids:
+                    return lobby_data
+
                 if len(players) >= max_players:
                     raise MaxPlayersReachedException("Lobby is full")
                 players.append({
@@ -94,6 +99,50 @@ class RedisHandler:
                     return lobby_data
                 else:
                     raise RevealNotReadyException("Not all players have made their choice")
+            except redis.WatchError:
+                continue
+            finally:
+                pipe.unwatch()
+
+    def get_lobby_status(self, lobby_key):
+        pipe = self.redis_client.pipeline()
+        while True:
+            try:
+                pipe.watch(lobby_key)
+                lobby_data = pipe.get(lobby_key)
+                if not lobby_data:
+                    raise LobbyNotFoundException("Lobby not found")
+                lobby_data = json.loads(lobby_data.decode())
+                reveal_cards = lobby_data['lobby_metadata'].get('reveal_cards', False)
+                players = lobby_data['players']
+
+                num_players = len(players)
+
+                round_number = lobby_data['lobby_metadata']['round_number']
+
+                game_status = {
+                    'num_players': num_players,
+                    'reveal_ready': reveal_cards,
+                    'round_number': round_number,
+                    'players': []
+                }
+
+                for player in players:
+                    if reveal_cards:
+                        player_info = {
+                            'player_id': player['player_id'],
+                            'card': player['choose_cards'][-1],
+                            'ready': True
+                        }
+                    else:
+                        player_info = {
+                            'player_id': player['player_id'],
+                            'card': None,
+                            'ready': True if player['choice_made'] else False
+                        }
+                    game_status['players'].append(player_info)
+
+                return game_status
             except redis.WatchError:
                 continue
             finally:
