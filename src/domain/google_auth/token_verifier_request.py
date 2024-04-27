c4d2495d9dc2705658dd16c19ca1a2d4e37e7784
Jan Szczuka <janogamesyt5@gmail.com>
2024-04-27 14:44:51 +0200

    Update token_verifier_request.py

--- src/domain/google_auth/token_verifier_request.py
+++ src/domain/google_auth/token_verifier_request.py
@@ -1,8 +1,6 @@
-from fastapi import Request
+from fastapi import Request, HTTPException
 from google.oauth2 import id_token
 from google.auth.transport import requests
-
-from src.contract.exceptions import InvalidTokenException, MissingTokenException
 
 CLIENT_ID = "50333286952-cn2jg51pihob9a8t22scs03a3gvtkpn3.apps.googleusercontent.com"
 
@@ -10,10 +8,10 @@ CLIENT_ID = "50333286952-cn2jg51pihob9a8t22scs03a3gvtkpn3.apps.googleusercontent
 def verify_token(request: Request):
     token = request.headers.get("Authorization", "").replace("Bearer ", "")
     if token == "":
-        raise MissingTokenException("Token is missing")
+        raise HTTPException(status_code=401, detail="Token is missing")
     try:
         user_info = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
         return user_info
     except ValueError:
-        raise InvalidTokenException("Invalid token")
+        raise HTTPException(status_code=401, detail="Invalid token")
 
